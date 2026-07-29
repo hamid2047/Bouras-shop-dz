ff33795814fa495f555724dabe4dfa8792fe80e3
hamid2047 <hamid50004000@gmail.com>
2026-07-29 07:39:40 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,35 @@
+/*==================================
+        BOURAS.SHOP_DZ
+        STORE CONFIG
+===================================*/
+
+
+export const STORE = {
+
+
+    name:
+    "Bouras.shop_dz",
+
+
+    currency:
+    "دج",
+
+
+    delivery:
+    "توصيل لجميع ولايات الجزائر",
+
+
+    payment:
+    "الدفع عند الاستلام",
+
+
+    whatsapp:
+    "",
+
+
+    defaultImage:
+    "assets/images/default.png"
+
+
+
+};
