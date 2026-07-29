f75b7231124bfb9d4519f157a75ba196772a55f1
hamid2047 <hamid50004000@gmail.com>
2026-07-29 07:44:19 +0100

    Create products.js

---
+++ data/products.js
@@ -0,0 +1,82 @@
+/*==================================
+        BOURAS.SHOP_DZ
+        PRODUCTS DATABASE
+===================================*/
+
+
+const PRODUCTS = [
+
+
+{
+id:1,
+
+name:"قميص رجالي فاخر",
+
+price:3500,
+
+oldPrice:4500,
+
+category:"clothes",
+
+image:"assets/products/product1.jpg",
+
+description:
+"قميص أنيق بتصميم عصري مناسب لجميع المناسبات",
+
+offer:true,
+
+stock:15
+},
+
+
+
+{
+id:2,
+
+name:"حذاء رياضي",
+
+price:6500,
+
+oldPrice:8000,
+
+category:"shoes",
+
+image:"assets/products/product2.jpg",
+
+description:
+"حذاء مريح بجودة عالية",
+
+offer:true,
+
+stock:10
+},
+
+
+
+{
+id:3,
+
+name:"عطر رجالي",
+
+price:4200,
+
+oldPrice:5000,
+
+category:"perfumes",
+
+image:"assets/products/product3.jpg",
+
+description:
+"رائحة فاخرة وثبات طويل",
+
+offer:false,
+
+stock:20
+}
+
+
+
+];
+
+
+export default PRODUCTS;
