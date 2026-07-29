9d25da47a0193f19f0dbec32c2ad18f077359572
hamid2047 <hamid50004000@gmail.com>
2026-07-29 06:49:27 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,74 +4,54 @@
 ===================================*/
 
 
-import { initializeApp } 
+import { initializeApp }
 from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 
 
-import { getFirestore } 
+import { getFirestore }
 from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
 
-import { getStorage } 
+import { getStorage }
 from "https://www.gstatic.com/firebasejs/12.16.0/firebase-storage.js";
 
 
 
 
+// Firebase Configuration
 
-//==================================
-// FIREBASE CONFIG
-// ضع معلومات مشروعك هنا
-//==================================
+const firebaseConfig = {
 
-
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
     apiKey: "AIzaSyBlqXOy9yOQ1Qpq0aW72MU6FfE5AmoF-JA",
+
     authDomain: "bouras-shop-dz.firebaseapp.com",
+
     projectId: "bouras-shop-dz",
+
     storageBucket: "bouras-shop-dz.firebasestorage.app",
+
     messagingSenderId: "975144083676",
+
     appId: "1:975144083676:web:af4f0ed1cdeb753e899419",
-    measurementId: "G-1J5C143B32"
-  };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+};
 
 
 
 
+// Initialize Firebase
 
-//==================================
-// INITIALIZE FIREBASE
-//==================================
-
-
-const app =
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 
+// Database
 
-const db =
-getFirestore(app);
-
-
-
-const storage =
-getStorage(app);
+const db = getFirestore(app);
 
 
+// Storage للصور
+
+const storage = getStorage(app);
 
 
 
